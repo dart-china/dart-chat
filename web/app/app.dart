d00487533c8b23950f0f887036b5e8b5ab9371d7
jarontai <jaroncn@gmail.com>
2016-03-17 17:00:45 +0800

    Add more selectors

--- web/app/app.dart
+++ web/app/app.dart
@@ -2,5 +2,60 @@ import 'package:angular2/angular2.dart';
 
 @Component(selector: 'dartlang-downloads', templateUrl: './app.html')
 class App {
-  List<String> gameConsoleList = ['PS4', 'XboxOne', 'WiiU'];
+  List<Map<String, String>> channelList = [
+    {'name': '稳定版', 'value': 'stable'},
+    {'name': 'dev版', 'value': 'dev'}
+  ];
+
+  List<String> platformList = ['windows', 'mac', 'linux'];
+
+  List<String> versionList = ['1.15.0', '1.14.2', '1.13.0'];
+
+  List<String> architectureList = ['64', '32'];
+
+  _Model model = new _Model();
+
+  App() {
+    model.channel = channelList[0]['value'];
+    model.platform = platformList[0];
+    model.version = versionList[0];
+    model.architecture = architectureList[0];
+  }
+
+  build() {
+    print(model);
+  }
+
+  onChannelChange(channel) {
+    model.channel = channel;
+  }
+
+  onPlatformChange(platform) {
+    model.platform = platform;
+  }
+
+  onVersionChange(version) {
+    model.version = version;
+  }
+
+  onArchitectureChange(architecture) {
+    model.architecture = architecture;
+  }
+}
+
+class _Model {
+  static String downloadUrl = '''https://storage.googleapis.com/dart-archive/
+  channels/<channel>/release/<release>/sdk/dartsdk-<platform>-<architecture>-release.zip''';
+  String channel;
+  String version;
+  String platform;
+  String architecture;
+
+  String buildDownloadUrl() {
+    return downloadUrl;
+  }
+
+  String toString() {
+    return '$channel $version $platform $architecture';
+  }
 }
