ec7146388a0c904ac469332c0ee2ac4e23f08419
daiqiu <daiqiu@appgame.com>
2016-03-31 14:31:55 +0800

    Follow the command line app convention

--- bin/server.dart
+++ bin/server.dart
@@ -1,22 +1,3 @@
-import 'dart:io';
+import 'package:dart_chat/dart_chat.dart' as server;
 
-main() async {
-  var server = await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080);
-  print("Serving at ${server.address}:${server.port}");
-  await for (HttpRequest request in server) {
-    if (request.uri.path == '/ws') {
-      // Upgrade an HttpRequest to a WebSocket connection.
-      var socket = await WebSocketTransformer.upgrade(request);
-      socket.listen(handleMsg);
-    }
-
-    request.response
-      ..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
-      ..write('Hello, world')
-      ..close();
-  }
-}
-
-handleMsg(msg) {
-  print('Message received: $msg');
-}
+void main(List<String> args) => server.main(args);
