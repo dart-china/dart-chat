36f95efc8959a78c6080cbe2088dc88deb3af337
jarontai <jaroncn@gmail.com>
2016-04-04 16:05:57 +0800

    Update server_test.dart, add package: test

--- test/server_test.dart
+++ test/server_test.dart
@@ -1,11 +1,34 @@
 import 'dart:io';
 import 'dart:convert';
+import 'dart:async';
 
 main(List<String> args) async {
-  var socket = await WebSocket.connect('ws://127.0.0.1:8080/ws');
-  socket.listen((msg) => print('Received $msg'));
-  _socketSend(socket, {
-    'message': {'text': 'test'}
+  Process.start('dart', ['bin/server.dart']).then(_runTest);
+}
+
+_runTest(Process process) async {
+  process.stdout.transform(UTF8.decoder).listen((data) {
+    print(data);
+    _setup();
+  });
+
+  process.stderr.transform(UTF8.decoder).listen((data) {
+    print(data);
+    process.kill();
+  });
+
+  new Timer(new Duration(seconds: 6), () {
+    process.kill();
+  });
+}
+
+_setup() {
+  new Timer(new Duration(seconds: 3), () async {
+    var socket = await WebSocket.connect('ws://127.0.0.1:8080/ws');
+    socket.listen((msg) => print('Received $msg'));
+    _socketSend(socket, {
+      'message': {'text': 'test'}
+    });
   });
 }
 
