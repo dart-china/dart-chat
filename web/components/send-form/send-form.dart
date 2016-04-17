6ac62a76ee4e80e92d7deb7bab522df06a91aed9
jarontai <jaroncn@gmail.com>
2016-04-17 22:46:59 +0800

    Upgrade angular to beta.15

--- web/components/send-form/send-form.dart
+++ web/components/send-form/send-form.dart
@@ -1,4 +1,4 @@
-import 'package:angular2/angular2.dart';
+import 'package:angular2/core.dart';
 
 @Component(selector: 'send-form', templateUrl: './send-form.html')
 class SendForm {
--- web/main.dart
+++ web/main.dart
@@ -1,4 +1,4 @@
-import 'package:angular2/bootstrap.dart';
+import 'package:angular2/platform/browser.dart';
 
 import 'components/dart-chat/dart-chat.dart';
 
