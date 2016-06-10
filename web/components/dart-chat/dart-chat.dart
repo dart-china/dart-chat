736a617f4388d515f04a9b6f12130f5280185788
jarontai <jaroncn@gmail.com>
2016-06-10 11:16:20 +0800

    Make message-panel display chat messages

--- web/components/dart-chat/dart-chat.dart
+++ web/components/dart-chat/dart-chat.dart
@@ -1,4 +1,5 @@
 import 'package:angular2/core.dart';
+import 'package:dart_chat/chat_client_js.dart';
 
 import '../message-panel/message-panel.dart';
 import '../send-form/send-form.dart';
@@ -9,10 +10,35 @@ import '../send-form/send-form.dart';
     directives: const [MessagePanel, SendForm])
 class DartChat {
   static final String _defaultRoom = 'Lobby';
+  static String _currentRoom = '';
   List<String> roomList = [_defaultRoom];
   List<String> messageList = [];
 
-  String get defaultRoom => _defaultRoom;
+  String get currentRoom => _currentRoom;
 
-  DartChat() {}
+  ChatClientJs client = new ChatClientJs();
+
+  DartChat() {
+    client.init('ws://127.0.0.1:9090/ws',
+        onMessage: _onMessage,
+        onRoomResult: _onRoomResult,
+        onNameResult: _onNameResult);
+  }
+
+  _onMessage(String message) {
+    messageList.add('message: $message');
+  }
+
+  _onRoomResult(bool success, String room) {
+    if (success) {
+      _currentRoom = room;
+      messageList.add('You joined $room');
+    }
+  }
+
+  _onNameResult(bool success, String name) {
+    if (success) {
+      messageList.add('You are known as $name');
+    }
+  }
 }
